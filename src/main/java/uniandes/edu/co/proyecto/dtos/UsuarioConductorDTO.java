4f7939d1c780d7a59c2dea6e5bfa5b98e9841f86
Juan Salazar <jf.salazar2@uniandes.edu.co>
2025-10-07 21:07:58 -0500

    cambios

--- src/main/java/uniandes/edu/co/proyecto/dtos/UsuarioConductorDTO.java
+++ src/main/java/uniandes/edu/co/proyecto/dtos/UsuarioConductorDTO.java
@@ -6,5 +6,9 @@ import lombok.Data;
 public class UsuarioConductorDTO extends UsuarioDTO {
     private Long id;
     private boolean disponible;
+    public String nombre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nombre'");
+    }
     
 }
